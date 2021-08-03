f57f4483ced53e4feca9fee858cf2b43b3159a3e
Joao Filipe <sfilipej@gmail.com>
2021-08-02 21:01:19 -0300

    HTTP methods added.

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const { response } = require('express');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const http = require('http');
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello Wolrd');
+    res.send('GET');
+});
+
+app.post('/', (req, res) => {
+    res.send('POST');
+});
+
+app.put('/', (req, res) => {
+    res.send('PUT');
+});
+
+app.delete('/', (req, res) => {
+    res.send('DELETE');
 });
 
 app.listen(port, () => {
