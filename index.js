24fb2fc54fb378199e8f42a195d0050a8c70a5e2
Joao Filipe <sfilipej@gmail.com>
2021-08-12 01:01:17 -0300

    method: put added

--- index.js
+++ index.js
@@ -6,29 +6,62 @@ app.use(express.json());
 
 const http = require('http');
 
-const usuarios = Array();
+var usuarios = Array();
+var livros = Array();
 
-app.get('/', (req, res) => {
+/* GET ALL */
+app.get('/usuarios', (req, res) => {
     res.status(200).json(usuarios);
 });
 
+/* GET FILTER */
+app.get('/usuario', (req, res) => {
+    const filtro = req.query.login;
+    const userFilter = usuarios.filter(function(item, index) {
+        return item.login === filtro
+    })
+    res.status(200).json(userFilter);
+});
+
+/* GET Work Hours - Time of reserve */
+app.get('/tempo', (req, res) => {
+    const total = livros.reduce(function(valorhoras, item){
+        return parseInt(valorhoras) + parseInt(item.valorhoras);
+    }, 0)
+    res.status(200).send("Valor: " + total);
+});
+
+/* Mapping, create a new array based on another */
+app.get('/map', (req, res) => {
+    const login = usuarios.map(function(item, index){
+        return item.login;
+    })
+    res.status(200).json(login);
+});
+
+/* Creating new user */
 app.post('/', (req, res) => {
     const user = req.body
-    console.log("Login: " + user.login);
-    console.log("Senha: " + user.senha);
-
     usuarios.push(user);
-
     console.log(usuarios);
     res.send('Adicionado com sucesso');
 });
 
+/* Updating user */
 app.put('/', (req, res) => {
+    const user = req.body
+    usuarios = usuarios.map(function(item, index){
+        if(user.login === item.login){
+            return user
+        }
+        return item;
+    })
+    console.log(usuarios);
     res.send('PUT');
 });
 
 app.delete('/', (req, res) => {
-    res.send('DELETE');
+    res.send('delete');    
 });
 
 app.listen(port, () => {
